5e148244c064b2f2c4d41e168b5fc062ec8c0ab5
Will Shown <willshown@microsoft.com>
2021-09-22 23:29:08 -0700

    Polish MainSection.

--- src/surfaces/MainSection.tsx
+++ src/surfaces/MainSection.tsx
@@ -1,23 +1,19 @@
-import { z } from 'zod'
-import { Section, sectionProps, SectionProps } from '../blocks/Section'
+import { Section, SectionProps } from '../blocks/Section'
 import { makeStyles } from '@fluentui/react-components'
 import { rem } from '../lib'
 
 const useStyles = makeStyles({
-  root: (theme) => {
-    console.log('theme', theme)
-    return {
+  root: (theme) => ({
       overflow: 'hidden',
       '--surface-background': theme.alias.color.neutral.neutralBackground3,
       '--surface-foreground': theme.alias.color.neutral.neutralForeground3,
       backgroundColor: 'var(--surface-background)',
       color: 'var(--surface-foreground)',
       paddingBlockEnd: rem(44),
-    }
-  },
+    }),
 })
 
 export const MainSection = (props: SectionProps) => {
   const styles = useStyles()
-  return <Section {...props} as="main" className={styles.root} />
+  return <Section {...props} as="main" level={1} className={styles.root} />
 }
