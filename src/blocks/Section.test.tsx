96b66b8d6709813b340455ce417d82eff3a6c43b
Will Shown <willshown@microsoft.com>
2021-09-24 17:00:22 -0700

    Adjust testing setup, scripts, and Storybook config.

--- src/blocks/Section.test.tsx
+++ src/blocks/Section.test.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import pw from 'playwright-core'
+/* eslint func-names: 0 */
+
 import expect from 'expect'
 import renderer from 'react-test-renderer'
 import { Section } from './Section'
 import get from 'lodash/get'
 
-describe('Section', () => {
-  it('accepts JSON props', async () => {
+describe('Section: unit tests', function () {
+  it('accepts JSON props', async function () {
     const section = renderer
       .create(
         <Section
@@ -17,5 +17,15 @@ describe('Section', () => {
       .toJSON()
 
     expect(get(section, ['children', 0, 'type'])).toEqual('h2')
+  })
+})
+
+describe('Section: interactions', function () {
+  before(async function (this) {
+    await this.page.goto(this.storybookUrl('tests-section--section-test'))
+  })
+
+  it('renders to the page', async function () {
+    expect(await this.page.locator('h2').count()).toEqual(1)
   })
 })
