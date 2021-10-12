7783a13a05140097fde25c139c1717a4a0afd8b3
Will Shown <willshown@microsoft.com>
2021-10-11 19:06:17 -0700

    Add Text tests.

--- src/inlines/Text/Text.test.tsx
+++ src/inlines/Text/Text.test.tsx
@@ -2,6 +2,16 @@
 
 import expect from 'expect'
 
-describe('Text: to do', function () {
-  it('has no tests yet')
+describe('Text: interactions', function () {
+  before(async function (this) {
+    await this.page.goto(this.storybookUrl('tests-text--text-test'))
+  })
+
+  it('renders to the page', async function () {
+    expect(
+      await this.page
+        .locator('text=4270bec2-4a76-4e13-899e-70115e1bf49e')
+        .count()
+    ).toEqual(1)
+  })
 })
