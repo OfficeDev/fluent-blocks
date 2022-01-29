af1fc596634571a28f8d90a5620f9198be67e93f
Will Shown <willshown@microsoft.com>
2022-01-28 16:02:06 -0800

    Add basic render test for ShortInputs.

--- src/blocks/ShortInputs/ShortInputs.test.tsx
+++ src/blocks/ShortInputs/ShortInputs.test.tsx
@@ -3,5 +3,21 @@
 import expect from 'expect'
 
 describe('Short inputs', function () {
-  it('iS READY FOR TESTS')
+  describe('interactions', function () {
+    this.timeout(5e3)
+    describe('using serializeable props', function () {
+      before(async function () {
+        await this.goto(this.storybookUrl('tests-sis--short-inputs-json-test'))
+      })
+      it('renders to the page', async function () {
+        expect(
+          await this.page
+            .locator(
+              '#root button >> text=37db439e-ea0f-44b6-8fbe-61138ad48601'
+            )
+            .count()
+        ).toEqual(1)
+      })
+    })
+  })
 })
