ec71957176df5115de3ed6019d2ae9c6a53719d3
Will Shown <willshown@microsoft.com>
2022-03-09 18:13:24 -0800

    Add React Toolbar schema.

---
+++ packages/react/src/blocks/Toolbar/Toolbar.tsx
@@ -0,0 +1,31 @@
+import {
+  toolbarProps as naturalToolbarProps,
+  toolbarDivider,
+  toolbarAction as naturalToolbarAction,
+} from '@fluentui/blocks-schemas'
+import { buttonProps } from '../../inputs'
+import { z } from 'zod'
+
+export const toolbarAction = naturalToolbarAction.merge(
+  buttonProps.omit({
+    variant: true,
+    size: true,
+    iconSize: true,
+  })
+)
+
+export const toolbarItemEntity = z.union([toolbarAction, toolbarDivider])
+
+export const toolbarItemSequence = z.array(toolbarItemEntity)
+
+export const toolbarProps = naturalToolbarProps.merge(
+  z.object({
+    toolbar: z.object({
+      items: toolbarItemSequence,
+    }),
+  })
+)
+
+export type ToolbarProps = z.infer<typeof toolbarProps>
+
+export const Toolbar = ({ toolbar: { items } }: ToolbarProps) => <div>Toolbar</div>
