bd46f1e9c5b46024cba4d1271c209d3554801c06
Will Shown <willshown@microsoft.com>
2022-03-22 16:51:20 -0700

    Fix schemas build to include types.

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -7,19 +7,3 @@ export * from './inputs'
 export * from './media'
 export * from './lib/actions'
 export * from './lib/themes'
-
-import blocks from './blocks/schemas'
-import exemplars from './exemplars/schemas'
-import inlines from './inlines/schemas'
-import inputs from './inputs/schemas'
-import media from './media/schemas'
-import lib from './lib/schemas'
-
-export const schemas: Record<string, ZodTypeAny> = {
-  ...blocks,
-  ...exemplars,
-  ...inlines,
-  ...inputs,
-  ...media,
-  ...lib,
-}
