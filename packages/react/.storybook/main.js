911ffff801c5124d11305c80230f6c6c02e578fe
Will Shown <willshown@microsoft.com>
2022-05-09 16:47:35 -0700

    Add dependencies to optimize before tests to fix CI.

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const tsconfig = path.resolve(__dirname, '../tsconfig.json')
+const { mergeConfig } = require('vite')
+const get = require('lodash/get')
 
 module.exports = {
   core: { builder: '@storybook/builder-vite' },
@@ -27,4 +29,25 @@ module.exports = {
     './addons/expand-all/register.js',
   ],
   staticDirs: ['./public'],
+  async viteFinal(config, { configType }) {
+    // return the customized config
+    return mergeConfig(config, {
+      // customize the Vite config here
+      optimizeDeps: {
+        include: [
+          ...get(config, ['optimizeDeps', 'include'], []),
+          '@storybook/react/dist/esm/client/docs/config',
+          '@storybook/react/dist/esm/client/preview/config',
+          '@storybook/addon-links/preview.js',
+          '@storybook/addon-docs/preview.js',
+          '@storybook/addon-actions/preview.js',
+          '@storybook/addon-measure/preview.js',
+          'faker/locale/en_US',
+          'lodash/range',
+          'lodash/find',
+          'chromatic/isChromatic',
+        ],
+      },
+    })
+  },
 }
