25c14cf2fbf67324fa205b6b00fe5364e4341509
Will Shown <willshown@microsoft.com>
2022-05-16 16:50:39 -0700

    Remove filter and find from topbar for now.

--- packages/schemas/types/surfaces/Topbar.d.ts
+++ packages/schemas/types/surfaces/Topbar.d.ts
@@ -1,13 +1,7 @@
-import { FilterProps } from '../blocks'
 import { MenuItemSequence } from '../lib/menu'
 
 interface TopbarFarProps {
   menu?: MenuItemSequence
-  filter?: FilterProps
-  /**
-   * An actionId
-   */
-  find?: string
 }
 
 interface TopbarNearProps {
