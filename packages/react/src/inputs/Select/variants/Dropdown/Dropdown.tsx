e146f63bb2b153c813312e4691dcd71f37a9c4bb
Will Shown <willshown@microsoft.com>
2022-06-14 16:05:21 -0700

    Refine Dropdown to behave similarly to the other Select variants.

--- packages/react/src/inputs/Select/variants/Dropdown/Dropdown.tsx
+++ packages/react/src/inputs/Select/variants/Dropdown/Dropdown.tsx
@@ -5,23 +5,51 @@ import { Select as FluentSelect } from '@fluentui/react-components/unstable'
 
 import { Paragraph } from '../../../../blocks'
 import { InlineContent } from '../../../../inlines'
-import { deleteInputValue, makeId, putInputValue } from '../../../../lib'
-import { SingleSelectProps } from '../../../../props'
+import {
+  deleteInputValue,
+  makeId,
+  putInputValue,
+  useFluentBlocksContext,
+} from '../../../../lib'
+import {
+  DescribedLabeledValueProps,
+  SingleSelectProps,
+} from '../../../../props'
+
+interface DescribedStringLabeledValueProps
+  extends Omit<DescribedLabeledValueProps, 'label'> {
+  label: string
+}
 
 export interface DropdownProps extends Omit<SingleSelectProps, 'select'> {
-  select: SingleSelectProps['select'] & {
+  select: Omit<SingleSelectProps['select'], 'options'> & {
     variant: 'combobox'
+    options: [
+      DescribedStringLabeledValueProps,
+      DescribedStringLabeledValueProps,
+      ...DescribedStringLabeledValueProps[]
+    ]
     multiple?: false
+    placeholder?: string
   }
   contextualLabelId?: string
   contextualDescriptionId?: string
 }
 
 export const Dropdown = ({
-  select: { disambiguatingLabel, description, actionId, initialValue, options },
+  select: {
+    disambiguatingLabel,
+    description,
+    actionId,
+    initialValue,
+    options,
+    placeholder,
+  },
   contextualLabelId,
   contextualDescriptionId,
 }: DropdownProps) => {
+  const { translations } = useFluentBlocksContext()
+
   useEffect(() => {
     putInputValue(actionId, initialValue || '')
     return () => deleteInputValue(actionId)
@@ -40,43 +68,58 @@ export const Dropdown = ({
   return (
     <>
       <FluentSelect
-        defaultValue={initialValue}
+        defaultValue={initialValue || ''}
         onChange={putSelectValue}
         {...(disambiguatingLabel
           ? { 'aria-label': disambiguatingLabel }
           : { 'aria-labelledby': contextualLabelId })}
         {...(description && { 'aria-describedby': contextualDescriptionId })}
       >
+        <option
+          value=""
+          disabled
+          {...(!initialValue && {
+            // React errors about this, but provides no alternative means of
+            // selecting the falsy placeholder value when `initialValue` is falsy.
+            selected: true,
+          })}
+        >
+          {placeholder || ' '}
+        </option>
         {options.map(({ value, label, description, descriptionVariant }) => {
           const optionDescriptionId = makeId(value, 'optionDescription')
           return (
-            <Fragment key={value}>
-              <option
-                {...{
-                  value,
-                  ...(description && {
-                    'aria-describedby': optionDescriptionId,
-                  }),
-                }}
-              >
-                <InlineContent inlines={label} />
-              </option>
-            </Fragment>
+            <option
+              {...{
+                key: value,
+                value,
+                ...(description && {
+                  'aria-describedby': optionDescriptionId,
+                }),
+              }}
+            >
+              {label}
+            </option>
           )
         })}
       </FluentSelect>
       {options.map(({ value, description }) => {
-        const optionDescriptionId = makeId(value, 'optionDescription')
-        return (
-          description && (
+        if (description) {
+          const optionDescriptionId = makeId(value, 'optionDescription')
+          return (
             <Paragraph
-              paragraph={description}
-              contextualId={optionDescriptionId}
-              contextualVariant="inputMeta--selectOption"
-              visuallyHidden
+              {...{
+                key: value,
+                paragraph: description,
+                contextualId: optionDescriptionId,
+                contextualVariant: 'inputMeta--selectOption',
+                visuallyHidden: true,
+              }}
             />
           )
-        )
+        } else {
+          return null
+        }
       })}
     </>
   )
